df7d3d15b2bff46af2587f858db09e33167a77c5
Lander Van laer <lander.vanlaer1@gmail.com>
2022-02-24 20:52:20 +0100

    update main.c

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define CONTRIBUTOR_NAME_LENGTH 21
-#define PROJECT_NAME_LENGTH 21
-#define SKILL_LENGTH 21
+#define STRLEN 21
+#define FILENAME "../resource/a_an_example.in.txt"
 
 typedef struct
 {
-    int id;
+    char name[STRLEN];
     short level;
 } Skill;
 
 typedef struct
 {
-    int id;
+    char skillName[STRLEN];
     int requiredLevel;
 } Role;
 
 typedef struct
 {
-    char name[CONTRIBUTOR_NAME_LENGTH];
-    Skill skills[];
+    char name[STRLEN];
+    int skillsLen;
+    Skill *skills;
 } Contributor;
 
 typedef struct
 {
-    char name[PROJECT_NAME_LENGTH];
+    char name[STRLEN];
     int numberOfDaysToComplete;
     int score;
     int bestBeforeDays;
@@ -32,13 +33,46 @@ typedef struct
     Role roles[];
 } Project;
 
-char *skills[SKILL_LENGTH];
-
 Contributor *contributors;
 Project *projects;
 
 int main()
 {
-    printf("Hello, World!\n");
+    FILE *fp;
+    fp = fopen(FILENAME, "r");
+
+    if (fp == NULL)
+    {
+        printf("File could not be read.");
+        exit(-1);
+    }
+
+    //LINE 1
+    int NContributors; // 1 <= N <= 100000
+    int NProjects;     // 1 <= N <= 100000
+    fscanf(fp, "%d%d%*c", &NContributors, &NProjects);
+
+    contributors = (Contributor *) malloc(NContributors);
+    projects = (Project *) malloc(NProjects);
+
+
+    for (int i = 0; i < NContributors; ++i)
+    {
+        Contributor contributor;
+        fscanf(fp, "%s%d%*c", contributor.name, &contributor.skillsLen);
+
+        contributor.skills = (Skill *) malloc(sizeof(Skill) * contributor.skillsLen);
+
+        for (int j = 0; j < contributor.skillsLen; ++j)
+        {
+            Skill skill;
+            fscanf(fp, "%s%hd%*c", skill.name, &skill.level);
+
+            contributor.skills[j] = skill;
+        }
+
+        contributors[i] = contributor;
+    }
+
     return 0;
 }
