60c59c6ef09addd84ac704b32efdae95a16f36ab
Lander Van laer <lander.vanlaer1@gmail.com>
2022-02-24 20:08:39 +0100

    structs

--- main.c
+++ main.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 
+#define CONTRIBUTOR_NAME_LENGTH 21
+#define PROJECT_NAME_LENGTH 21
+#define SKILL_LENGTH 21
+
+typedef struct
+{
+    int id;
+    short level;
+} Skill;
+
+typedef struct
+{
+    int id;
+    int requiredLevel;
+} Role;
+
+typedef struct
+{
+    char name[CONTRIBUTOR_NAME_LENGTH];
+    Skill skills[];
+} Contributor;
+
+typedef struct
+{
+    char name[PROJECT_NAME_LENGTH];
+    int numberOfDaysToComplete;
+    int score;
+    int bestBeforeDays;
+    int rolesSize;
+    Role roles[];
+} Project;
+
+char *skills[SKILL_LENGTH];
+
+Contributor *contributors;
+Project *projects;
+
 int main()
 {
     printf("Hello, World!\n");
